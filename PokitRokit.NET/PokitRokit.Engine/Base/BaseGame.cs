f6f2a0817da8c881bd0ff40cc88ee4f6e7a5428f
TheAOS <felixhollqvist@gmail.com>
2014-10-16 21:56:08 +0200

    Base classes and structure

---
+++ PokitRokit.NET/PokitRokit.Engine/Base/BaseGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokitRokit.Engine.Base
+{
+    public abstract class BaseGame
+    {
+        public List<BasePlayer> Players { get; set; }
+        public Guid ID { get; set; }
+
+        public BaseGame()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
---
+++ PokitRokit.NET/PokitRokit.Engine/TexasHoldEm/Game.cs
@@ -0,0 +1,13 @@
+﻿using PokitRokit.Engine.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PokitRokit.Engine.TexasHoldEm
+{
+    public class Game : BaseGame
+    {
+
+    }
+}
